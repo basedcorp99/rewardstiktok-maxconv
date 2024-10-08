170617d31355d3689401f7a4cf02811afb1d9d47
basedcorp99 <basedcorp99@gmail.com>
2024-10-08 18:10:59 +0200

    fix small bugs in subid. fix 37 mappings

--- functions/37/[[catchall]].js
+++ functions/37/[[catchall]].js
@@ -2,5 +2,5 @@ import user37Mappings from '../../config/37.json';
 import { handleRequest } from '../../lib/handleRequest.js'
 
 export async function onRequest(context) {
-  return handleRequest(context, user37Mappings.mappings);
+  return handleRequest(context, user37Mappings);
 }

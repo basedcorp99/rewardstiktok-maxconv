1df243ac29993f9f717a0c52dda4b562d2e2655e
basedcorp99 <basedcorp99@gmail.com>
2024-10-08 16:10:24 +0200

    add links, refactor handler

--- functions/anon/[[catchall]].js
+++ functions/anon/[[catchall]].js
@@ -2,5 +2,5 @@ import userAnonMappings from '../../config/anon.json';
 import { handleRequest } from '../../lib/handleRequest.js'
 
 export async function onRequest(context) {
-  return handleRequest(context, userAnonMappings.mappings);
+  return handleRequest(context, userAnonMappings);
 }

8eec4742dd968426c772b92c232d77cfa3f149eb
basedcorp99 <basedcorp99@gmail.com>
2024-10-08 15:22:12 +0200

    refactor

---
+++ functions/37/[[catchall]].js
@@ -0,0 +1,6 @@
+import user37Mappings from '../../config/37.json';
+import { handleRequest } from '../../lib/handleRequest.js'
+
+export async function onRequest(context) {
+  return handleRequest(context, user37Mappings.mappings);
+}
---
+++ functions/anon/[[catchall]].js
@@ -0,0 +1,6 @@
+import userAnonMappings from '../../config/anon.json';
+import { handleRequest } from '../../lib/handleRequest.js'
+
+export async function onRequest(context) {
+  return handleRequest(context, userAnonMappings.mappings);
+}

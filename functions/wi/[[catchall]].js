ef70bdf6a0bf1575b9d8ac03cc33417ee835da6c
chupapigangster <94854542+chupapigangster@users.noreply.github.com>
2025-01-04 19:30:52 +0100

    Added pixel ID in willie catchall.js

--- functions/wi/[[catchall]].js
+++ functions/wi/[[catchall]].js
@@ -2,5 +2,5 @@ import userwillieMappings from '../../config/willie.json';
 import { handleImageLPwPixel } from '../../lib/handleImageLP.js'
 
 export async function onRequest(context) {
-  return handleImageLPwPixel(context, userwillieMappings, '');
+  return handleImageLPwPixel(context, userwillieMappings, 'CTSNS23C77U1GH2BKSHG');
 }

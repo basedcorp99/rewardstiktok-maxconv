da6719034e51e12b0dfb073aa56623003c1d59b9
basedcorp99 <basedcorp99@gmail.com>
2025-02-03 13:29:12 +0100

    add static route

---
+++ functions/[lp].js
@@ -0,0 +1,33 @@
+import { wrapReq } from '../lib/wrapper.js'
+
+import { userMap } from '../config/userMap.js'
+import { handlerMap } from '../config/handlerMap.js'
+
+
+export async function onRequest(context) {
+    const url = new URL(context.request.url);
+    const s = url.searchParams;
+
+    const lp = context.params.lp;
+    const user = s.get('u');
+
+    if (user === null) {
+        throw new Error(`Missing required parameter: u`);
+    }
+
+    const requiredParams = ['pl', 'g', 'o', 's1'];
+
+    // Extract parameters and validate
+    const params = requiredParams.map(param => {
+        const value = s.get(param);
+        if (value === null) {
+            throw new Error(`Missing required parameter: ${param}`);
+        }
+        return value;
+    });
+
+    let usermappings = userMap[user];
+    let cb = handlerMap[lp];
+
+    return cb(params, usermappings);
+}

a24e51e2f8611b30c35ee28751e93857808b56eb
basedcorp99 <basedcorp99@gmail.com>
2025-02-19 20:22:28 +0100

    tweak meta pixel

--- assets/scripts/FBpixel.js
+++ assets/scripts/FBpixel.js
@@ -1,4 +1,5 @@
 export default `
+<script>
 !function(f,b,e,v,n,t,s)
 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
 n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -9,4 +10,9 @@ s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
 fbq('init', '{{pixel_id}}');
 fbq('track', 'PageView');
+</script>
+<noscript>
+  <img height="1" width="1" style="display:none" 
+       src="https://www.facebook.com/tr?id={{pixel_id}}&ev=PageView&noscript=1"/>
+</noscript>
 `;

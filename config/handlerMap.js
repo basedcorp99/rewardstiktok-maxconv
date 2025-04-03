875221c532696ad8d5e3ad4dc18933a948061bb4
gitejea <github@ejeacapital.com>
2025-04-03 18:00:43 +0200

    Pushed new lander + shein, cash & ttshop lander

--- config/handlerMap.js
+++ config/handlerMap.js
@@ -1,6 +1,7 @@
 import { handleQuizLP } from '../lib/page-handlers/handleQuizLP.js'
 import { handleDynLP } from '../lib/page-handlers/handleDynLP.js'
 import { handleAppLP } from '../lib/page-handlers/handleAppLP.js'
+import { handleAppLPv2 } from '../lib/page-handlers/handleAppLPv2.js'
 import { handleImageLP } from '../lib/page-handlers/handleImageLP.js'
 import { handleQuiz2LP } from '../lib/page-handlers/handleQuiz2LP.js'
 import { handleDCLP } from '../lib/page-handlers/handleDCLP'
@@ -10,6 +11,7 @@ export const handlerMap =
     'quiz': handleQuizLP,
     'dynlan': handleDynLP,
     'app': handleAppLP,
+    'appv2': handleAppLPv2,
     'img': handleImageLP,
     'quiz2': handleQuiz2LP,
     'dc': handleDCLP,

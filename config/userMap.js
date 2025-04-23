01c5128f96ed4a371317d936f1e9033d831b1bb8
basedcorp99 <basedcorp99@gmail.com>
2025-04-23 22:32:25 +0200

    fix

--- config/userMap.js
+++ config/userMap.js
@@ -1,10 +1,11 @@
 import userAnonMappings from './anon.json';
+import userFmMappings from './fm.json';
 import user37Mappings from './37.json';
 import userWillieMappings from './willie.json';
 
 export const userMap = {
     'ab': userAnonMappings,
-    'fm': userAnonMappings,
+    'fm': userFmMappings,
     '37': user37Mappings,
     'wi': userWillieMappings,
 }

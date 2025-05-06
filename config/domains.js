c8f76ba48540f023217b28574402ae98edb21006
basedcorp99 <basedcorp99@gmail.com>
2025-05-06 12:21:00 +0200

    add glitchy apple

---
+++ config/domains.js
@@ -0,0 +1,16 @@
+// src/config/domains.js
+export const domains = [
+    "bluemandillo.com",
+    "greenleopards.com",
+    "portlandspecc.shop",
+    "ryojishop.com",
+    "pauliewalnutstore.com",
+    "redmonkfruit.com",
+    "whitelobstercloth.com",
+    "whitelobsterwatch.com",
+    "longmeterisland.com",
+    "hikorushop.com",
+    "rewardstt.com",
+    "claim-tt.com",
+    "tokrewards.shop",
+]
